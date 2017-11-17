e2a8753eafba37d62433bb16c9bef7dd20aa19a8
Martin Vanek <mvanek@email.cz>
2017-11-17 16:30:56 +0100

    changes for angular >=1.6.2

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 An Angular directive for nested forms that prevents `$invalid` from propagating, allowing a nested form to be invalid without affecting its parent's validity.
  
 Code originally written by [91K00](https://github.com/91K00), as seen [here](http://stackoverflow.com/questions/19333544/skip-nested-forms-validation-with-angularjs/24936234#24936234) and [here](https://github.com/angular/angular.js/issues/5858#issuecomment-50026221).
+
+The original code does not work for AngularJS >=1.6.2. So it did modification based on the latest comments [here](https://github.com/angular/angular.js/issues/5858#issuecomment-344326469)
  
  **Usage:**
 
@@ -16,4 +18,4 @@ Code originally written by [91K00](https://github.com/91K00), as seen [here](htt
  
 ~~~
 
-**Example:** [http://jsfiddle.net/gikoo/qNrFX/](http://jsfiddle.net/gikoo/qNrFX/)
+**Example:** [https://codepen.io/MOmega/pen/eeePPW](https://codepen.io/MOmega/pen/eeePPW)
--- index.js
+++ index.js
@@ -7,28 +7,10 @@ angular.module('isolateForm', []).directive('isolateForm', [function () {
         return;
       }
 
-      // Do a copy of the controller
-      var ctrlCopy = {};
-      angular.copy(ctrl, ctrlCopy);
-
       // Get the parent of the form
       var parent = elm.parent().controller('form');
       // Remove parent link to the controller
       parent.$removeControl(ctrl);
-
-      // Replace form controller with a "isolated form"
-      var isolatedFormCtrl = {
-        $setValidity: function (validationToken, isValid, control) {
-          ctrlCopy.$setValidity(validationToken, isValid, control);
-          parent.$setValidity(validationToken, true, ctrl);
-        },
-        $setDirty: function () {
-          elm.removeClass('ng-pristine').addClass('ng-dirty');
-          ctrl.$dirty = true;
-          ctrl.$pristine = false;
-        },
-      };
-      angular.extend(ctrl, isolatedFormCtrl);
     }
   };
 }]);
